72e026985f87367f4bb5eed86dcb6933a8214edd
Amir Saghaleini <EEDEA.Media.Group@gmail.com>
2022-11-24 20:07:25 +0330

    2.1: Course information step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,63 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => {
+  const { name, exercises, id } = part;
+  return (
+    <p key={id}>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    {/* <Total parts={course.parts} /> */}
+  </div>
+);
+
+// const Total = ({ parts }) => (
+//   <p>
+//     Number of exercises{" "}
+//     {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+//   </p>
+// );
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
