61549b66c5c5d1907e66a584ee5f6627c152d111
Amir Saghaleini <EEDEA.Media.Group@gmail.com>
2022-11-25 18:34:54 +0330

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -15,10 +15,14 @@ const Persons = ({ persons }) => (
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "09123456789" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchString, setSearchString] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -40,9 +44,18 @@ const App = () => {
   const numberChangeHandler = (event) => {
     setNewNumber(event.target.value);
   };
+  const searchStringChangeHandler = (event) => {
+    setSearchString(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with
+        <input value={searchString} onChange={searchStringChangeHandler} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={nameChangeHandler} />
@@ -55,7 +68,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons
+        persons={persons.filter(
+          (person) =>
+            searchString === "" ||
+            RegExp(`${searchString}`, "i").test(person.name)
+        )}
+      />
     </div>
   );
 };
